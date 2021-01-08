1e4b2f707d946a441ac285ea70509d3aaa7f8995
MrSandrow <dr.breewster@gmail.com>
2021-01-08 20:26:19 +0100

    .

--- slider.js
+++ slider.js
@@ -1,85 +1,29 @@
-// Variables
+// Event Listeners
 
-const moon = document.querySelector(".moon");
-const emojis = document.querySelector(".emojis");
-const second = document.querySelector(".second");
-const counter = document.querySelector(".counter");
-let number = 0;
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 
-// Event Listeners
-
-moon.addEventListener("click", theme);
-emojis.addEventListener("mouseover", randomEmoji);
-second.addEventListener("mousemove", slider);
-counter.addEventListener("click", count);
 next.addEventListener("click", () => slide(-1));
 prev.addEventListener("click", () => slide(1));
+window.addEventListener("wheel", defile);
 
-// Functions
-
-function theme() {
-  document.documentElement.classList.toggle("dark");
-}
-
-function randomEmoji() {
-  const characters = [
-    "😂",
-    "😅",
-    "😄",
-    "😎",
-    "😍",
-    "🤩",
-    "🤑",
-    "😲",
-    "😨",
-    "😇",
-    "🤯",
-    "😮",
-    "🤐",
-    "😫",
-    "🤤",
-    "😤",
-    "😬",
-    "🥳",
-  ];
-  emojis.innerText = characters[Math.floor(Math.random() * characters.length)];
-}
-
-function slider(e) {
-  const body = document.querySelector("body");
-  const container = document.querySelector(".second .container");
-  const width = (body.offsetWidth - container.offsetWidth) / 2;
-  const img2 = document.querySelector(".img2");
-  let x = e.clientX - width;
-
-  if (x < 0) {
-    x = 0;
-  }
-
-  img2.style.left = x + "px";
-}
-
-function count() {
-  const span = document.querySelector(".third span");
-  number = number + 1;
-
-  if (number === 1) {
-    span.innerText = `${number} Clic`;
+function defile(event) {
+  if (event.deltaY < 0) {
+    slide(-1);
   } else {
-    span.innerText = `${number} Clics`;
+    slide(1);
   }
 }
 
-// Slider
+// Add images to the HTML-markup
 
-const sliderWrapper = document.querySelector(".fourth .container");
+const sliderWrapper = document.getElementById("sliderWrapper");
 let parts = [];
 let images = ["img/day.jpg", "img/night.jpg"];
 let current = 0;
 
-let part = document.querySelector(".part");
+let part = document.createElement("div");
+part.className = "part";
 
 let section = document.createElement("div");
 section.className = "section";
@@ -89,11 +33,11 @@ img.src = images[current];
 
 section.appendChild(img);
 part.appendChild(section);
+sliderWrapper.appendChild(part);
 
 parts.push(part);
 
 // Slide handler
-console.log(part.childNodes);
 let playing = false;
 
 const slide = (dir) => {
