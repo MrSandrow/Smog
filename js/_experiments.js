f0142d71f7176bc4c4420f61d8d0bbbb5a6bf3eb
MrSandrow <dr.breewster@gmail.com>
2021-03-28 18:51:01 +0200

    .

--- js/_experiments.js
+++ js/_experiments.js
@@ -22,6 +22,132 @@ function slideSection() {
   experimentsFirst.style.transform = `translateX(${translated}px)`;
 }
 
+// Fireworks
+
+const fireworksContainer = document.querySelector(".experiments-first .container");
+const canvasEl = document.querySelector(".fireworks");
+const ctx = canvasEl.getContext("2d");
+const colors = ["#dc0000", "#f56656", "#ffc700", "#e05600"];
+const numberOfParticules = 30;
+
+// var tap = "ontouchstart" in window || navigator.msMaxTouchPoints ? "touchstart" : "mousedown";
+
+function setCanvasSize() {
+  canvasEl.width = fireworksContainer.offsetWidth * 2;
+  canvasEl.height = fireworksContainer.offsetHeight * 2;
+  canvasEl.style.width = fireworksContainer.offsetWidth + "px";
+  canvasEl.style.height = fireworksContainer.offsetHeight + "px";
+  canvasEl.getContext("2d").scale(2, 2);
+}
+
+function setParticuleDirection(p) {
+  var angle = (anime.random(0, 360) * Math.PI) / 180;
+  var value = anime.random(50, 180);
+  var radius = [-1, 1][anime.random(0, 1)] * value;
+  return {
+    x: p.x + radius * Math.cos(angle),
+    y: p.y + radius * Math.sin(angle),
+  };
+}
+
+function createParticule(x, y) {
+  var p = {};
+  p.x = x;
+  p.y = y;
+  p.color = colors[anime.random(0, colors.length - 1)];
+  p.radius = anime.random(16, 32);
+  p.endPos = setParticuleDirection(p);
+  p.draw = function () {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    ctx.fillStyle = p.color;
+    ctx.fill();
+  };
+  return p;
+}
+
+function createCircle(x, y) {
+  var p = {};
+  p.x = x;
+  p.y = y;
+  p.color = "#FFF";
+  p.radius = 0.1;
+  p.alpha = 0.5;
+  p.lineWidth = 6;
+  p.draw = function () {
+    ctx.globalAlpha = p.alpha;
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    ctx.lineWidth = p.lineWidth;
+    ctx.strokeStyle = p.color;
+    ctx.stroke();
+    ctx.globalAlpha = 1;
+  };
+  return p;
+}
+
+function renderParticule(anim) {
+  for (var i = 0; i < anim.animatables.length; i++) {
+    anim.animatables[i].target.draw();
+  }
+}
+
+function animateParticules(x, y) {
+  const circle = createCircle(x, y);
+  const particules = [];
+
+  for (let i = 0; i < numberOfParticules; i++) {
+    particules.push(createParticule(x, y));
+  }
+
+  anime({
+    targets: particules,
+    x: (p) => p.endPos.x,
+    y: (p) => p.endPos.y,
+    radius: 0.1,
+    duration: anime.random(1200, 1800),
+    easing: "easeOutExpo",
+    update: renderParticule,
+  });
+
+  anime({
+    targets: circle,
+    radius: anime.random(80, 160),
+    lineWidth: 0,
+    alpha: {
+      value: 0,
+      easing: "linear",
+      duration: anime.random(600, 800),
+    },
+    duration: anime.random(1200, 1800),
+    easing: "easeOutExpo",
+    update: renderParticule,
+    offset: 0,
+  });
+}
+
+var render = anime({
+  duration: Infinity,
+  update: function () {
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  },
+});
+
+fireworksContainer.addEventListener("mousedown", function (e) {
+  const topSpace = fireworksContainer.getBoundingClientRect().top;
+  const pointerX = e.clientX || e.touches[0].clientX;
+  const pointerY = (e.clientY || e.touches[0].clientY) - topSpace;
+
+  render.play();
+  animateParticules(pointerX, pointerY);
+});
+
+var centerX = window.innerWidth / 2;
+var centerY = window.innerHeight / 2;
+
+setCanvasSize();
+window.addEventListener("resize", setCanvasSize);
+
 // Filters
 
 const characters = [];
